5d87b19df2eaf9a7fea610d5ea74b9049e2a3fa8
Giovanne Dias <giovanne@ids.com.br>
2015-06-19 12:40:32 -0300

    Initial Commit

---
+++ BannerTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BannerTrack
+//
+//  Created by Giovanne Dias on 6/19/15.
+//  Copyright (c) 2015 Giovanne Dias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BannerTrack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BannerTrack
+//
+//  Created by Giovanne Dias on 6/19/15.
+//  Copyright (c) 2015 Giovanne Dias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
